d8eb37d15335c8e931770492bbded30c18777cce
Anurag kumawat <anuragkumawat1603@gmail.com>
2025-07-31 19:12:48 +0530

    feedback

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,10 +1,9 @@
+from rest_framework.renderers import JSONRenderer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-
 class StatusView(APIView):
     permission_classes = [AllowAny]
-
+    renderer_classes = [JSONRenderer]
     def get(self, request):
         return Response({"status": "ok"})
